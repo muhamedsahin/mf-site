3193e6f8fd49e6f349831c6b0a05e3f99b674624
muhamedsahin <muhamedsahin378@gmail.com>
2021-05-07 18:45:59 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ export default function App() {
       <div />
       <div class="meraba">
         <h1>MFŞ</h1>
+        <h2>MY name mfş</h2>
+        <h2>my job web desing</h2>
+        <h2>Hello boro</h2>
+        <h2>which</h2>
+        <button class="js-click">start</button>
         <div class="profil">
           <img src="https://github.com/muhamedsahin/resimlerim/blob/main/png-transparent-svg-animation-computer-icons-rocket-icon-purple-company-cartoon-removebg-preview%20(2).png?raw=true" />
         </div>
