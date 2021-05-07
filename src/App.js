bf4dae7ffef1076761c5aada8d9d734d153b11e4
muhamedsahin <muhamedsahin378@gmail.com>
2021-05-07 18:33:38 +0300

    2 files modified

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ export default function App() {
     <>
       <div />
       <div class="meraba">
-        <h1>mfş</h1>
+        <h1>MFŞ</h1>
+        <div class="profil">
+          <img src="https://github.com/muhamedsahin/resimlerim/blob/main/png-transparent-svg-animation-computer-icons-rocket-icon-purple-company-cartoon-removebg-preview%20(2).png?raw=true" />
+        </div>
+      </div>
+      <div class="profilarkapilan">
+        <h1></h1>
       </div>
     </>
   );
